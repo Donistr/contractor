80d4f19da923a6fae6899b794f070fdee15951f7
Donistr <deniselag@mail.ru>
2024-07-07 11:26:51 +0400

    Initial commit

---
+++ src/main/java/org/example/contractor/ContractorApplication.java
@@ -0,0 +1,13 @@
+package org.example.contractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/contractor/ContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

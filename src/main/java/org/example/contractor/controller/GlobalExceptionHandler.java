f341bb3356b1a828e62c22c7c3b673353987a5b8
Donistr <deniselag@mail.ru>
2024-07-08 01:11:12 +0400

    feat: добавил GlobalExceptionHandler для обработки ошибок

---
+++ src/main/java/org/example/contractor/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package org.example.contractor.controller;
+
+import org.example.contractor.dto.ErrorObject;
+import org.example.contractor.exception.BaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorObject> handle(BaseException exception) {
+        return new ResponseEntity<>(new ErrorObject(exception.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+}

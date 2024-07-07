7a3863c2810bfccc2afd3cc98b5dc4f6f0e0736d
Donistr <deniselag@mail.ru>
2024-07-07 11:32:50 +0400

    feat: добавил checkstyle и убрал лишнее из pom.xml

--- src/test/java/org/example/contractor/ContractorApplicationTests.java
+++ src/test/java/org/example/contractor/ContractorApplicationTests.java
@@ -1,13 +1,7 @@
 package org.example.contractor;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ContractorApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
